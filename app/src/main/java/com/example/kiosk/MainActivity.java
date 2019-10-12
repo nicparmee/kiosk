7d8895265b68477aa1ab93b68b80005cb08d1172
nicparmee <nparmee@gmail.com>
2019-10-12 14:04:43 +0200

    all changes

--- app/src/main/java/com/example/kiosk/MainActivity.java
+++ app/src/main/java/com/example/kiosk/MainActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.num.numandroidpagecurleffect.PageCurlView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton webBtn2;
     private ImageButton webBtn3;
 
-
+    private Button closeWeb;
 
     private ViewPager pdfsliding;
 
@@ -42,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PageCurlView pageCurlView = (PageCurlView) findViewById(R.id.pagecurl_view);
+
+
+        List<Integer> pages_id = new ArrayList<>();
+        pages_id.add(R.drawable.bglogo);
+        pages_id.add(R.drawable.bglogo);
+
+        pageCurlView.setCurlView(pages_id);
+        pageCurlView.setCurlSpeed(65);
 
         vv = (VideoView) findViewById(R.id.videoView);
 
@@ -66,6 +81,10 @@ public class MainActivity extends AppCompatActivity {
         vv.start();
 
 
+        webView1 = findViewById(R.id.web1);
+        webView2 = findViewById(R.id.web2);
+        webView3 = findViewById(R.id.web3);
+
 
         webView1 = findViewById(R.id.web1);
         webView1.loadUrl("https://www.fundamentum.co.za/");
@@ -73,15 +92,59 @@ public class MainActivity extends AppCompatActivity {
         webView1.setWebViewClient(new WebViewClient());
 
 
+        webView2 = findViewById(R.id.web2);
+        webView2.loadUrl("https://boogertmanandpartners.com/");
+        webView2.getSettings().setJavaScriptEnabled(true);
+        webView2.setWebViewClient(new WebViewClient());
+
+
+        webView3 = findViewById(R.id.web3);
+        webView3.loadUrl("https://www.mdsarch.co.za/");
+        webView3.getSettings().setJavaScriptEnabled(true);
+        webView3.setWebViewClient(new WebViewClient());
+
         webBtn1 = findViewById(R.id.web1Btn);
+        webBtn2 = findViewById(R.id.web2Btn);
+        webBtn3 = findViewById(R.id.web3Btn);
+        closeWeb = findViewById(R.id.closeWeb);
 
 
         webBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 webView1.setVisibility(View.VISIBLE);
+                closeWeb.setVisibility(View.VISIBLE);
             }
         });
+
+        webBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView2.setVisibility(View.VISIBLE);
+                closeWeb.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        webBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView3.setVisibility(View.VISIBLE);
+                closeWeb.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        closeWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView1.setVisibility(View.GONE);
+                webView2.setVisibility(View.GONE);
+                webView3.setVisibility(View.GONE);
+                closeWeb.setVisibility(View.GONE);
+            }
+        });
+
 
     }
 }
